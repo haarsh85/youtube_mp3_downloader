26f4ef3110849be0af36d612393de888c872f858
Harsha Nanayakkara <harsh.life@hotmail.com>
2022-10-05 16:38:55 +0530

    Added customized download location

--- youtube_mp3.py
+++ youtube_mp3.py
@@ -1,19 +1,25 @@
 from pytube import YouTube
 import os
 
+path = 'D:\Music\Mix\\'
+
 video_url = input("Please enter the video URL: ")
 yt = YouTube(video_url)
 
 #Get highest bitrate audio stream for given codec (defaults to mp4)
 audio = yt.streams.get_audio_only()
 
-audio.download()
+audio.download(output_path=path)
 
 file_name = audio.default_filename
 
+source = path + file_name
+
+print(source)
+
 if ' ' in file_name:
-    os.rename(file_name, file_name.replace(' ', '_'))
-    file_name = file_name.replace(' ','_')
+    os.rename(source, source.replace(' ', '_'))
+    file_name = source.replace(' ','_')
 
 file_without_ext = os.path.splitext(file_name)[0]
 
